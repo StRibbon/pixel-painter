9d3bbba28fb89e887cfb058be77076ad34c94909
Stephen Ribbon <stribbon@icloud.com>
2015-05-15 15:41:12 -0700

    add div row and div function

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
+var createDiv = function (color){
+	var newDiv = document.createElement('DIV');
+		newDiv.style.width = '30px';
+		newDiv.style.height = '30px';
+		newDiv.style.padding = '1px';
+		newDiv.style.float = 'left';		
+		newDiv.style.border = '1px black solid';
+		newDiv.style.backgroundColor = color;
+		return newDiv;
+
+}
+
 document.onload = function pixelPaint(){
-	console.log("Yo, Welcome to my Pixel Painter app");
+	alert("Yo, Welcome to my Pixel Painter app");
+	
 	var container = document.createElement('container');
 	container.style.width = '300px';
 	container.style.position = 'fixed';
+
 	document.body.appendChild(container);
 	createGrid(container);
 	createPaint(container);
 
 }();
+
 var paintBrush = "white";
 
 function createGrid(elementToAppend){
 	for (var i = 0; i < 104; i++){
 
-	var newDiv = document.createElement('div');		
-	newDiv.style.width = '30px';
-	newDiv.style.height = '30px';
-	newDiv.style.padding = '1px';
-	newDiv.style.float = 'left';		
-	newDiv.style.border = '1px black solid';
-	newDiv.addEventListener('click', function(){
-		this.style.backgroundColor = paintBrush;
-	})
-	elementToAppend.appendChild(newDiv);
+		var newDiv = createDiv("white");
+
+		if(i>95){
+			newDiv.style.backgroundColor = "white";
+			newDiv.style.borderColor = "white";	
+		} else {
+			newDiv.addEventListener('click', function(){
+				this.style.backgroundColor = paintBrush;
+			})
+		}
+		elementToAppend.appendChild(newDiv);
 	}
 }
 
 function createPaint(elementToAppend){
 	
-	var colorArr = ["white", "red", "orange", "yellow", "green", "aqua", "blue", "purple"]
+	var colorArr = ["white", "red", "orange", "yellow", "green", "blue", "aqua", "purple"];
 	for (var i = 0; i < 8; i++){
-	var newDiv = document.createElement('div');
+		var newDiv =document.createElement('div');
 		newDiv.style.backgroundColor = colorArr[i];	
 		newDiv.style.width = '30px';
 		newDiv.style.height = '30px';
@@ -40,9 +56,10 @@ function createPaint(elementToAppend){
 
 		newDiv.addEventListener("click",function(){
 			paintBrush = this.style.backgroundColor;
-		})
-	elementToAppend.appendChild(newDiv);
+		});
+		elementToAppend.appendChild(newDiv);
 	}
 }
+
 
 
